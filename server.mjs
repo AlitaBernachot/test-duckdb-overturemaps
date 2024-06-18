bf5896df12077b283eede345ce7854bc89e5c1c6
AlitaBernachot <alita.bernachot@camptocamp.com>
2024-06-18 18:54:18 +0200

    fix: content type geojson

--- server.mjs
+++ server.mjs
@@ -27,6 +27,7 @@ app.get('/', async (req, res) => {
   `;
 
   const data = await doRequest(query, fileName);
+  res.setHeader('content-type', 'application/geo+json');
   res.send(data);
 });
 
@@ -56,6 +57,7 @@ app.get('/countries', async (req, res) => {
   `;
 
   const data = await doRequest(query, fileName);
+  res.setHeader('content-type', 'application/geo+json');
   res.send(data);
 });
 
@@ -84,10 +86,35 @@ app.get('/places', async (req, res) => {
     LIMIT 10000
   `;
 
+  // At lease more than 1 hour to get the results
+  // SELECT
+  //     id,
+  //     names.primary AS primary_names,
+  //     categories.main AS primary_category,      
+  //     sources[1].dataset AS primary_source,
+  //     confidence,
+  //     websites[1] AS website,
+  //     phones[1] AS phone,
+  //     addresses[1].freeform AS address,
+  //     addresses[1].postcode AS postcode,
+  //     addresses[1].region AS region,
+  //     addresses[1].country AS country,
+  //     ST_GeomFromWkb(geometry) AS geometry
+  //   FROM 
+  //     read_parquet('s3://overturemaps-us-west-2/release/2024-06-13-beta.0/theme=places/type=place/*', filename=true, hive_partitioning=1) places
+  //   WHERE 
+  //   	ST_Within(
+	//     	ST_GeomFromWKB(geometry), 
+	//     	ST_GeomFromGeoJSON('{"type":"Polygon","coordinates":  [[[54.4, 24.3],[54.8, 24.3],[54.8, 24.5],[54.4, 24.5],[54.4, 24.3]]]}')
+  //   	)
+  //   /*ORDER BY RANDOM()
+  //   LIMIT 20*/
+
   // [[[5.9559111595,45.8179931641],[10.4920501709,45.8179931641],[10.4920501709,47.808380127],[5.9559111595,47.808380127],[5.9559111595,45.8179931641]]]
   // [[[54.4, 24.3],[54.8, 24.3],[54.8, 24.5],[54.4, 24.5],[54.4, 24.3]]]
 
   const data = await doRequest(query, fileName);
+  res.setHeader('content-type', 'application/geo+json');
   res.send(data);
 });
 
